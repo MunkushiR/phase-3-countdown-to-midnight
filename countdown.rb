54510221b9267661f1498484075c0671c958745f
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-05-21 14:55:50 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(number)
+    # number = 365
+
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        countdown_with_sleep()
+        number -= 1
+    end
+
+    puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds = 3)
+    sleep seconds
+end
+
+binding.pry
